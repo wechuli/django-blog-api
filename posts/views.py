f774558351e71b9e90a4af53ec0c707476812cb0
wechuli <wechulipaul@yahoo.com>
2019-01-10 18:58:35 +0300

    adding permissions for views to authenticated users

--- posts/views.py
+++ posts/views.py
@@ -1,15 +1,18 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 # Create your views here.
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
 
 class PostList(generics.ListCreateAPIView):
+ 
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
